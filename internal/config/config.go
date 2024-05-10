1939de1deb87920f5975b678bb6ad29133c9f7d3
Юрий Черепахин <y.cherepakhin@nic.ru>
2024-05-10 20:42:26 +0500

    config hbm

--- internal/config/config.go
+++ internal/config/config.go
@@ -23,8 +23,9 @@ type Config struct {
 
 func MustLoad() *Config {
 	// Получаем путь до конфиг-файла из env-переменной CONFIG_PATH
-	// configPath := os.Getenv("CONFIG_PATH")
-	configPath := "../../config/local/yaml"
+	//$ENV:CONFIG_PATH="E:\workgo\goshell\config\local.yaml"
+	configPath := os.Getenv("CONFIG_PATH")
+	// configPath := "../../config/local/yaml"
 
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH environment variable is not set")
